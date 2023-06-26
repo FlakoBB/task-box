2e445c1c4fd58fa023fc236a2b06872bdc296c08
FlakoBB <josephrbb99@gmail.com>
2023-06-26 14:07:48 -0600

    Funcion para agregar tarea a la lista

--- src/components/taskForm.jsx
+++ src/components/taskForm.jsx
@@ -1,31 +1,51 @@
+'use client'
+
+import { useRef, useState } from 'react'
+import { useLoggedContext } from '@/context/LoggedContext'
 import styles from '@/styles/forms.module.scss'
 
 const TaskForm = () => {
-  const addTask = (event) => {
+  const { addTask } = useLoggedContext()
+
+  const taskTitle = useRef()
+  const taskDescription = useRef()
+  const taskPriority = useRef()
+
+  const [incrementId, setIncrementId] = useState(0)
+
+  const handleSubmit = (event) => {
     event.preventDefault()
+    const title = taskTitle.current.value
+    const description = taskDescription.current.value
+    const priority = taskPriority.current.value
+    addTask(incrementId, title, description, priority, false)
+    setIncrementId(incrementId + 1)
+    taskTitle.current.value = ''
+    taskDescription.current.value = ''
+    taskPriority.current.value = 'Normal'
     alert('Nueva tarea agregada')
   }
 
   return (
     <article className={styles.form_container}>
       <h2 className={styles.form_title}>Nueva Tarea</h2>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.input_group}>
           <label className={styles.label} htmlFor='title'>Titulo:</label>
-          <input className={styles.field} type='text' id='title' />
+          <input className={styles.field} type='text' id='title' ref={taskTitle} />
         </div>
         <div className={styles.input_group}>
           <label className={styles.label} htmlFor='description'>Descripcion:</label>
-          <input className={styles.field} type='text' id='description' />
+          <input className={styles.field} type='text' id='description' ref={taskDescription} />
         </div>
         <div className={styles.input_group}>
           <label className={styles.label} htmlFor='priority'>Prioridad:</label>
-          <select className={styles.select} id='priority'>
+          <select className={styles.select} id='priority' ref={taskPriority}>
             <option>Normal</option>
             <option>Urgente</option>
           </select>
         </div>
-        <button className={styles.btn_form} onClick={addTask}>Agregar</button>
+        <button className={styles.btn_form}>Agregar</button>
       </form>
     </article>
   )
--- src/context/LoggedContext.jsx
+++ src/context/LoggedContext.jsx
@@ -6,50 +6,65 @@ const LoggedContext = createContext()
 
 export const LogggedProvider = ({ children }) => {
   // ToDo: poner la lista de tareas como un estado para agregar, eliminar y modificar tareas
-  const tasksList = [
-    {
-      id: 1,
-      title: 'Tarea 1',
-      description: 'Descripcionde la trarea 1',
-      priority: 'Normal',
-      state: false
-    },
-    {
-      id: 2,
-      title: 'Tarea 2: Titulo largo para ver como se comporta el overview en el item de tarea',
-      description: 'Descripcionde la trarea 2',
-      priority: 'Urgente',
-      state: false
-    },
-    {
-      id: 3,
-      title: 'Tarea 3',
-      description: 'Esta es la descripcion improvisada de la TAREA 3. Es una descripcion larga para ver como se comporta el campo a diferentes tamañosd de descripciones. espero que con esto sea suficioente, si no tendre que regresar a escribir más.',
-      priority: 'Normal',
-      state: true
-    },
-    {
-      id: 4,
-      title: 'Tarea 4',
-      description: 'Descripcionde la trarea 4',
-      priority: 'Urgente',
-      state: false
-    },
-    {
-      id: 5,
-      title: 'Tarea 5',
-      description: 'Descripcionde la trarea 5',
-      priority: 'Normal',
-      state: false
-    },
-    {
-      id: 6,
-      title: 'Tarea 6',
-      description: 'Descripcionde la trarea 6',
-      priority: 'Normal',
-      state: false
-    }
-  ]
+  // ? Estado lista de tareas
+  const [tasksList, setTasksList] = useState([])
+
+  const addTask = (id, title, description, priority, state = false) => {
+    setTasksList([
+      ...tasksList,
+      {
+        id,
+        title,
+        description,
+        priority,
+        state
+      }
+    ])
+  }
+  // const tasksList = [
+  //   {
+  //     id: 1,
+  //     title: 'Tarea 1',
+  //     description: 'Descripcionde la trarea 1',
+  //     priority: 'Normal',
+  //     state: false
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'Tarea 2: Titulo largo para ver como se comporta el overview en el item de tarea',
+  //     description: 'Descripcionde la trarea 2',
+  //     priority: 'Urgente',
+  //     state: false
+  //   },
+  //   {
+  //     id: 3,
+  //     title: 'Tarea 3',
+  //     description: 'Esta es la descripcion improvisada de la TAREA 3. Es una descripcion larga para ver como se comporta el campo a diferentes tamañosd de descripciones. espero que con esto sea suficioente, si no tendre que regresar a escribir más.',
+  //     priority: 'Normal',
+  //     state: true
+  //   },
+  //   {
+  //     id: 4,
+  //     title: 'Tarea 4',
+  //     description: 'Descripcionde la trarea 4',
+  //     priority: 'Urgente',
+  //     state: false
+  //   },
+  //   {
+  //     id: 5,
+  //     title: 'Tarea 5',
+  //     description: 'Descripcionde la trarea 5',
+  //     priority: 'Normal',
+  //     state: false
+  //   },
+  //   {
+  //     id: 6,
+  //     title: 'Tarea 6',
+  //     description: 'Descripcionde la trarea 6',
+  //     priority: 'Normal',
+  //     state: false
+  //   }
+  // ]
 
   // ? Estado de nombre de usuario
   const [username, setUsername] = useState('')
@@ -69,7 +84,7 @@ export const LogggedProvider = ({ children }) => {
   }
 
   return (
-    <LoggedContext.Provider value={{ loggedIn, login, logout, tasksList, obtainUsername, username }}>
+    <LoggedContext.Provider value={{ loggedIn, login, logout, tasksList, obtainUsername, username, addTask }}>
       {children}
     </LoggedContext.Provider>
   )
